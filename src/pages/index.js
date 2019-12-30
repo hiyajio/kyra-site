78fa94af35cf2885f4b0963eb86119e88b97b298
lockjio <jio.buenviaje@gmail.com>
2019-12-30 13:12:10 -0500

    Start on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,36 @@ import React from "react"
 
 export default () => (
   <div className="flex">
-    <div className="flex-1 bg-golden font-body h-screen w-2/5 flex-1"></div>
-    <div className="flex-none bg-silvered font-body h-screen w-1/5 flex-1">
+    <div className="flex-1 bg-golden font-body h-screen w-2/5 overflow-hidden">
+      <video
+        autoplay="true"
+        loop="true"
+        muted="true"
+        className="object-cover bg-auto min-w-full h-full"
+      >
+        <source
+          src={`../../videos/juanaSkirtRGB.webm`}
+          alt="Juana Skirt Colored"
+          type="video/webm"
+        />
+      </video>
+    </div>
+    <div className="flex-none bg-silvered font-body h-screen w-1/5 text-gray-400">
       <ul>
-        <li>Kyra</li>
-        <li>Collections</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>KYRA</li>
+        <li>COLLECTIONS</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
       </ul>
     </div>
-    <div className="flex-1 bg-bronzed font-body h-screen w-2/5 flex-1"></div>
+    <div className="flex-1 bg-silvered font-body h-screen w-2/5">
+      <video autoplay="true" loop="true" muted="true" className="h-full">
+        <source
+          src={`../../videos/juanaSkirtBW.webm`}
+          alt="Juana Skirt BnW"
+          type="video/webm"
+        />
+      </video>
+    </div>
   </div>
 )
