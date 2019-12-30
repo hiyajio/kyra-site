cace8ef3fe30fb11b639e9cf4b4899981faf014a
lockjio <jio.buenviaje@gmail.com>
2019-12-30 02:31:58 -0500

    Add boiler plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,52 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Kyra Buenviaje`,
+    description: `Personal website of Kyra Gabrielle Buenviaje: fashion designer.`,
+    author: `@lockjio`,
+    siteUrl: `https://kyrabuenviaje.com/`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `kyra-site`,
+    //     short_name: `kyra`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#ffffff`,
+    //     display: `minimal-ui`,
+    //     icon: `src/favicon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+    {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "en",
+      },
+    },
+    `gatsby-plugin-smoothscroll`,
+    `gatsby-plugin-favicon`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+    //     trackingId: "UA-150731209-1",
+    //   },
+    // },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-offline`,
+  ],
 }
