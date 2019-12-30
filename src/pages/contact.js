81ba1ebb9167302209862be876956a0d35f51a36
lockjio <jio.buenviaje@gmail.com>
2019-12-30 18:40:16 -0500

    Connect contact form to formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -89,9 +89,9 @@ export default () => (
       </div>
     </div>
     <div className="flex-1 bg-white font-body h-screen w-2/5">
-      <div className="mt-10">
+      <div className="absolute bottom-0 mb-8 w-full">
         <form
-          action="https://formspree.io/FORM_ID"
+          action="https://formspree.io/xrgeoooy"
           method="POST"
           className="w-full max-w-lg"
         >
@@ -107,6 +107,7 @@ export default () => (
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
                 type="text"
+                name="firstName"
               />
             </div>
             <div className="w-full md:w-1/2 px-3">
@@ -117,24 +118,26 @@ export default () => (
                 /last name
               </label>
               <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-last-name"
                 type="text"
+                name="lastName"
               />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
               <label
-                className="block tracking-wide text-gray-700 text-sm mb-2"
+                className="block tracking-wide text-silvered text-sm mb-2"
                 for="grid-password"
               >
-                /email
+                /email address
               </label>
               <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block w-full bg-gray-200 text-silvered border border-silvered rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-silvered"
                 id="email"
                 type="email"
+                name="_replyto"
               />
             </div>
           </div>
@@ -147,7 +150,7 @@ export default () => (
                 /message
               </label>
               <textarea
-                className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
                 id="message"
               ></textarea>
             </div>
@@ -156,7 +159,7 @@ export default () => (
             <div className="md:w-1/3">
               <button
                 className="shadow bg-golden opacity-50 hover:opacity-100 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                type="button"
+                type="submit"
               >
                 send
               </button>
