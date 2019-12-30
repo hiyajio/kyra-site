aa8437d69498e8f32bb4a413c9997832107e9dcb
lockjio <jio.buenviaje@gmail.com>
2019-12-30 14:21:16 -0500

    Add resume to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div className="flex">
@@ -16,15 +17,39 @@ export default () => (
         />
       </video>
     </div>
-    <div className="flex-none bg-silvered font-body h-screen w-1/5 text-gray-400">
-      <ul>
-        <li>KYRA</li>
-        <li>COLLECTIONS</li>
-        <li>ABOUT</li>
-        <li>CONTACT</li>
-      </ul>
+    <div className="flex bg-white font-body h-screen w-1/5 text-gray-700 overflow-visible">
+      <img
+        src={`../../images/logo.png`}
+        alt="Kyra logo"
+        className="absolute w-8/12 top-0 right-0"
+      ></img>
+      <div className="justify-center content-center object-center m-auto">
+        <ul>
+          <li className="hover:text-white">
+            <Link to="#home">Home</Link>
+          </li>
+          <li className="hover:text-white">
+            <Link to="#collections">Collections</Link>
+          </li>
+          <li className="hover:text-white">
+            <a
+              href={`../../files/resumeBuenviaje.pdf`}
+              rel="noopener noreferrer"
+              target="_BLANK"
+            >
+              Resume
+            </a>
+          </li>
+          <li className="hover:text-white">
+            <Link to="#about">About</Link>
+          </li>
+          <li className="hover:text-white">
+            <Link to="#contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div className="flex-1 bg-silvered font-body h-screen w-2/5">
+    <div className="flex-1 bg-white font-body h-screen w-2/5">
       <video autoplay="true" loop="true" muted="true" className="h-full">
         <source
           src={`../../videos/juanaSkirtBW.webm`}
