1409d35fc20c32e7017fc645718404cac57906ae
lockjio <jio.buenviaje@gmail.com>
2019-12-30 12:32:14 -0500

    Initialize home screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default () => (
-  <div className="bg-blue-100 font-body text-bronzed">Hello world!</div>
+  <div className="flex">
+    <div className="flex-1 bg-golden font-body h-screen w-2/5 flex-1"></div>
+    <div className="flex-none bg-silvered font-body h-screen w-1/5 flex-1">
+      <ul>
+        <li>Kyra</li>
+        <li>Collections</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+    <div className="flex-1 bg-bronzed font-body h-screen w-2/5 flex-1"></div>
+  </div>
 )
