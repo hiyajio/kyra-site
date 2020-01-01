b13152a583d55906cd3bc977bb9ea7b1fd7d5757
lockjio <jio.buenviaje@gmail.com>
2020-01-01 17:59:57 -0500

    Fix contact alt label

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,12 +12,12 @@ export default () => (
       >
         <source
           src={`../../videos/juanaVestBW.webm`}
-          alt="Juana Skirt BW webm"
+          alt="Juana Vest BW webm"
           type='video/webm; codecs="vp8, vorbis"'
         />
         <source
           src={`../../videos/juanaVestBW.mp4`}
-          alt="Juana Skirt BW mp4"
+          alt="Juana Vest BW mp4"
           type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
         />
       </video>
