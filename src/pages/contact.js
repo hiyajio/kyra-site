e13040a83a4c250d940c89d757e8ec63d751e120
lockjio <jio.buenviaje@gmail.com>
2020-01-03 18:22:53 -0500

    Revise watermark

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -190,14 +190,14 @@ export default () => (
     </div>
     <div className="absolute bottom-0 right-0 mb-1 mr-1 text-golden font-body text-xs">
       <p>
-        made by{" "}
+        site by{" "}
         <a
           href="https://jbuenviaje.com/"
           rel="noopener noreferrer"
           target="_BLANK"
           className="hover:text-white hover:bg-silvered"
         >
-          jbuenviaje
+          /jbuenviaje
         </a>
       </p>
     </div>
