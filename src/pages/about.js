a990451f285cd0d8d3a64a2a7094fdfd59cbfa6e
lockjio <jio.buenviaje@gmail.com>
2020-01-03 22:00:35 -0500

    Implement Header component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
+
+import Header from "../components/Header/index.js"
 
 export default () => (
   <div className="flex">
@@ -55,101 +56,7 @@ export default () => (
         </p>
       </div>
     </div>
-    <div className="flex bg-white font-body h-screen w-1/5 text-gray-700 overflow-hidden">
-      <picture className="absolute w-8/12 top-0 right-0">
-        <source srcset={`../../images/logo.webp`} type="image/webp" />
-        <img src={`../../images/logo.png`} alt="Kyra logo" />
-      </picture>
-      <div className="justify-center content-center object-center m-auto">
-        <ul>
-          <Link to="/">
-            <li className="hover:text-white hover:bg-silvered">/home</li>
-          </Link>
-          <div className="dropdown relative">
-            <li className="hover:text-white hover:bg-silvered">/collections</li>
-            <div className="dropdown-content relative overflow-hidden h-0">
-              <Link to="juanaPartOneC">
-                <li className="hover:text-white hover:bg-silvered">
-                  juana part 1 - curated
-                </li>
-              </Link>
-              <Link to="juanaPartOne">
-                <li className="hover:text-white hover:bg-silvered">
-                  juana part 1
-                </li>
-              </Link>
-              <Link to="hold">
-                <li className="hover:text-white hover:bg-silvered">hold</li>
-              </Link>
-              <Link to="spaceVegan">
-                <li className="hover:text-white hover:bg-silvered">
-                  space vegan
-                </li>
-              </Link>
-              <Link to="grandmasCouch">
-                <li className="hover:text-white hover:bg-silvered">
-                  grandma's couch
-                </li>
-              </Link>
-            </div>
-          </div>
-          <a
-            href={`../../files/resumeBuenviaje.pdf`}
-            rel="noopener noreferrer"
-            target="_BLANK"
-          >
-            <li className="hover:text-white hover:bg-silvered">/resume</li>
-          </a>
-          <Link to="about">
-            <li className="hover:text-white hover:bg-silvered">/about</li>
-          </Link>
-          <Link to="contact">
-            <li className="hover:text-white hover:bg-silvered">/contact</li>
-          </Link>
-        </ul>
-        <div className="block absolute w-full">
-          <a
-            href="https://instagram.com/kyrainstudio"
-            rel="noopener noreferrer"
-            target="_BLANK"
-            className="hover:opacity-0"
-          >
-            <img
-              src={`../../images/logos/instagram.svg`}
-              alt={"Kyra on Instagram"}
-              aria-label="Instagram"
-              className="inline-block h-4 mr-2 opacity-50"
-            />
-          </a>
-          <a
-            href="https://linkedin.com/in/kyrabuenviaje"
-            rel="noopener noreferrer"
-            target="_BLANK"
-            className="opacity-50 hover:opacity-0"
-          >
-            <img
-              src={`../../images/logos/linkedin.svg`}
-              alt={"Kyra on LinkedIn"}
-              aria-label="LinkedIn"
-              className="inline-block h-4 mr-2"
-            />
-          </a>
-          <a
-            href="mailto:kyrabuenviaje@gmail.com"
-            rel="noopener noreferrer"
-            target="_BLANK"
-            className="opacity-50 hover:opacity-0"
-          >
-            <img
-              src={`../../images/logos/email.svg`}
-              alt={"Kyra on Gmail"}
-              aria-label="Gmail"
-              className="inline-block h-4 mr-2"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
+    <Header />
     <div className="flex-1 bg-white font-body h-screen w-2/5 overflow-hidden">
       <picture className="mt-16">
         <source srcset={`../../images/kyra.webp`} type="image/webp" />
