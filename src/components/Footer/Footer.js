577c61ef766f2b909013e958c7dafe19e33db37e
lockjio <jio.buenviaje@gmail.com>
2020-01-07 00:52:29 -0800

    Fix z-index

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = props => {
   return (
-    <div className="fixed bottom-0 right-0 mb-1 mr-1 text-silvered font-body text-xs opacity-25">
+    <div className="z-50 fixed bottom-0 right-0 mb-1 mr-1 text-silvered font-body text-xs opacity-25">
       <p>&copy; 2020 Kyra Buenviaje. All rights reserved.</p>
     </div>
   )
