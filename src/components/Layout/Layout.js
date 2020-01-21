8195007b975777b33cee71e6e62517c389b522e7
lockjio <jio.buenviaje@gmail.com>
2020-01-21 01:25:49 -0500

    Test mobile scaling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,7 +11,7 @@ const Layout = props => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta name="author" content="Kyra Buenviaje" />
         <meta property="og:type" content="website" />
