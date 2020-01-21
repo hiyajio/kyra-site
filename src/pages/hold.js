581f664ef8fb342e84fd117ee9ed232e657ac3a4
lockjio <jio.buenviaje@gmail.com>
2020-01-20 23:56:05 -0500

    Finish collection responsive mobile layout

--- src/pages/hold.js
+++ src/pages/hold.js
@@ -21,13 +21,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold191.png`}
             alt="Hold 1"
-            style={{
-              position: "absolute",
-              height: "auto",
-              width: "40rem",
-              left: "6rem",
-              top: "6rem",
-            }}
+            className="one-ho"
           />
         </picture>
         <picture>
@@ -38,13 +32,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold192.png`}
             alt="Hold 2"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "64rem",
-              top: "8rem",
-            }}
+            className="two-ho abs"
           />
         </picture>
         <picture>
@@ -55,13 +43,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold193.png`}
             alt="Hold 3"
-            style={{
-              position: "absolute",
-              height: "32rem",
-              width: "auto",
-              left: "2rem",
-              top: "43rem",
-            }}
+            className="three-ho abs"
           />
         </picture>
         <picture>
@@ -72,13 +54,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold194.png`}
             alt="Hold 4"
-            style={{
-              position: "absolute",
-              height: "40rem",
-              width: "auto",
-              left: "60rem",
-              top: "73rem",
-            }}
+            className="four-ho abs"
           />
         </picture>
         <picture>
@@ -89,13 +65,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold195.png`}
             alt="Hold 5"
-            style={{
-              position: "absolute",
-              height: "32rem",
-              width: "auto",
-              left: "41rem",
-              top: "99rem",
-            }}
+            className="five-ho abs"
           />
         </picture>
         <picture>
@@ -106,13 +76,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold196.png`}
             alt="Hold 6"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "12rem",
-              top: "91rem",
-            }}
+            className="six-ho abs"
           />
         </picture>
         <picture>
@@ -123,13 +87,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold197.png`}
             alt="Hold 7"
-            style={{
-              position: "absolute",
-              height: "40rem",
-              width: "auto",
-              left: "22rem",
-              top: "129rem",
-            }}
+            className="seven-ho abs"
           />
         </picture>
         <picture>
@@ -140,13 +98,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold198.png`}
             alt="Hold 8"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "2rem",
-              top: "154rem",
-            }}
+            className="eight-ho abs"
           />
         </picture>
         <picture>
@@ -157,13 +109,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold199.png`}
             alt="Hold 9"
-            style={{
-              position: "absolute",
-              height: "32rem",
-              width: "auto",
-              left: "41rem",
-              top: "174rem",
-            }}
+            className="nine-ho abs"
           />
         </picture>
         <picture>
@@ -174,13 +120,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1910.png`}
             alt="Hold 10"
-            style={{
-              position: "absolute",
-              height: "32rem",
-              width: "auto",
-              left: "8rem",
-              top: "194rem",
-            }}
+            className="ten-ho abs"
           />
         </picture>
         <picture>
@@ -191,13 +131,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1911.png`}
             alt="Hold 11"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "15rem",
-              top: "224rem",
-            }}
+            className="eleven-ho abs"
           />
         </picture>
         <picture>
@@ -208,13 +142,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1912.png`}
             alt="Hold 12"
-            style={{
-              position: "absolute",
-              height: "40rem",
-              width: "auto",
-              left: "59rem",
-              top: "229rem",
-            }}
+            className="twelve-ho abs"
           />
         </picture>
         <picture>
@@ -225,13 +153,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1913.png`}
             alt="Hold 13"
-            style={{
-              position: "absolute",
-              height: "32rem",
-              width: "auto",
-              left: "40rem",
-              top: "259rem",
-            }}
+            className="thirteen-ho abs"
           />
         </picture>
         <picture>
@@ -242,13 +164,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1914.png`}
             alt="Hold 14"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "10rem",
-              top: "274rem",
-            }}
+            className="fourteen-ho abs"
           />
         </picture>
         <picture>
@@ -259,13 +175,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1915.png`}
             alt="Hold 15"
-            style={{
-              position: "absolute",
-              height: "32rem",
-              width: "auto",
-              left: "41rem",
-              top: "290rem",
-            }}
+            className="fifteen-ho abs"
           />
         </picture>
         <picture>
@@ -276,13 +186,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1916.png`}
             alt="Hold 16"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "4rem",
-              top: "309rem",
-            }}
+            className="sixteen-ho abs"
           />
         </picture>
         <picture>
@@ -293,13 +197,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1917.png`}
             alt="Hold 17"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "60rem",
-              top: "335rem",
-            }}
+            className="seventeen-ho abs"
           />
         </picture>
         <picture>
@@ -310,13 +208,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1918.png`}
             alt="Hold 18"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "20rem",
-              top: "355rem",
-            }}
+            className="eighteen-ho abs"
           />
         </picture>
         <picture>
@@ -327,13 +219,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1919.png`}
             alt="Hold 19"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "55rem",
-              top: "370rem",
-            }}
+            className="nineteen-ho abs"
           />
         </picture>
         <picture>
@@ -344,15 +230,7 @@ const HoldPage = props => {
           <img
             src={`../../images/collections/hold/kbHold1920.png`}
             alt="Hold 20"
-            style={{
-              position: "absolute",
-              height: "36rem",
-              width: "auto",
-              left: "4rem",
-              top: "387rem",
-              zIndex: "-1",
-              paddingBottom: "5rem",
-            }}
+            className="twenty-ho abs"
           />
         </picture>
         <HeaderFolio bgColor="bg-transparent z-50" />
