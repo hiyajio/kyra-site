94f78c8b728c496e194dccfcc014e510234e84c3
lockjio <jio.buenviaje@gmail.com>
2020-02-07 11:27:06 -0500

    Automate copyright year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,9 @@ import React from "react"
 const Footer = props => {
   return (
     <div className="z-50 fixed bottom-0 right-0 mb-1 mr-1 text-silvered font-body text-xs opacity-25">
-      <p>&copy; 2020 Kyra Buenviaje. All rights reserved.</p>
+      <p>
+        &copy; {new Date().getFullYear()} Kyra Buenviaje. All rights reserved.
+      </p>
     </div>
   )
 }
