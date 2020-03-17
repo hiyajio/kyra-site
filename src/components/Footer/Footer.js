21dd8e9646a4eea6d7e87aa8b12bf629582b2db3
lockjio <jio.buenviaje@gmail.com>
2020-03-17 17:14:27 -0400

    Fix copyright footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,8 @@ const Footer = props => {
   return (
     <div className="z-50 fixed bottom-0 right-0 mb-1 mr-1 text-silvered font-body text-xs opacity-25">
       <p>
-        &copy; {new Date().getFullYear()} Kyra Buenviaje. All rights reserved.
+        Copyright &copy; {new Date().getFullYear()} Kyra Buenviaje. All rights
+        reserved.
       </p>
     </div>
   )
