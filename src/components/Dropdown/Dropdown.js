87120846b2e0521d67dc61ce6948efa59a0a9c67
lockjio <jio.buenviaje@gmail.com>
2020-03-19 21:56:51 -0400

    Add aria labels to dropdown for PWA audit

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -10,31 +10,31 @@ const Dropdown = props => {
         <li className="hover:text-white hover:bg-silvered">/collections</li>
       </ul>
       <div className="dropdown-content relative overflow-hidden h-0">
-        <Link to="juanaPartOneC">
+        <Link to="juanaPartOneC" aria-label="Juana 1 curated">
           <ul>
             <li className="hover:text-white hover:bg-silvered">
               juana part i - curated
             </li>
           </ul>
         </Link>
-        <Link to="juanaPartOne">
+        <Link to="juanaPartOne" aria-label="Juana 1 simple">
           <ul>
             <li className="hover:text-white hover:bg-silvered">
               juana part i - 100% recycled denim
             </li>
           </ul>
         </Link>
-        <Link to="hold">
+        <Link to="hold" aria-label="Hold">
           <ul>
             <li className="hover:text-white hover:bg-silvered">hold</li>
           </ul>
         </Link>
-        <Link to="spaceVegan">
+        <Link to="spaceVegan" aria-label="Space vegan">
           <ul>
             <li className="hover:text-white hover:bg-silvered">space vegan</li>
           </ul>
         </Link>
-        <Link to="grandmasCouch">
+        <Link to="grandmasCouch" aria-label="Grandma's couch">
           <ul>
             <li className="hover:text-white hover:bg-silvered">
               grandma's couch
