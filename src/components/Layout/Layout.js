3efd88b77a7df5cc0b1e7bbb8ae1bf6ee6f53724
lockjio <jio.buenviaje@gmail.com>
2020-03-28 01:24:33 -0400

    Update metadata image

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,13 +13,24 @@ const Layout = props => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <title>Kyra Buenviaje</title>
         <meta name="author" content="Kyra Buenviaje" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Kyra Buenviaje" />
+
+        <meta property="og:title" content="Kyra Buenviaje" />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/50670255/76829260-1c726c80-67f9-11ea-84b2-3b134ff8eb9d.png"
+        />
+        <meta
+          property="og:description"
+          content="Portfolio website of Kyra Gabrielle Buenviaje: Clothing Enthusiast, Fashion Designer, Artist, Creative and proud Filipino."
+        />
         <meta property="og:url" content="kyrabuenviaje.com" />
+
         <meta property="twitter:url" content="kyrabuenviaje.com" />
         <meta name="theme-color" content="#ffffff" />
-        <title>Kyra Buenviaje</title>
         <meta property="og:title" content="Kyra Buenviaje" />
         <meta property="twitter:title" content="Kyra Buenviaje" />
         <meta
