706c654cc5a3d8031a3726d56205a8d004991c0a
lockjio <jio.buenviaje@gmail.com>
2020-04-21 21:31:17 -0400

    Fix updated collection layout

--- src/pages/juanaPartOne.js
+++ src/pages/juanaPartOne.js
@@ -19,64 +19,163 @@ const JuanaPartOnePage = props => {
             type="image/webp"
           />
           <img
-            src={`../../images/collections/juanaPartOne/kbJuana1.png`}
+            src={`../../images/collections/juanaPartOne/Juana1.png`}
             alt="Juana part one 1"
             className="one-js abs"
           />
         </picture>
         <picture>
           <source
-            srcset={`../../images/collections/juanaPartOne/kbJuana2.webp`}
+            srcset={`../../images/collections/juanaPartOne/Juana2.webp`}
             type="image/webp"
           />
           <img
-            src={`../../images/collections/juanaPartOne/kbJuana2.png`}
+            src={`../../images/collections/juanaPartOne/Juana2.png`}
             alt="Juana part one 2"
             className="two-js abs"
           />
         </picture>
         <picture>
           <source
-            srcset={`../../images/collections/juanaPartOne/kbJuana3.webp`}
+            srcset={`../../images/collections/juanaPartOne/Juana3.webp`}
             type="image/webp"
           />
           <img
-            src={`../../images/collections/juanaPartOne/kbJuana3.png`}
+            src={`../../images/collections/juanaPartOne/Juana3.png`}
             alt="Juana part one 3"
             className="three-js abs"
           />
         </picture>
         <picture>
           <source
-            srcset={`../../images/collections/juanaPartOne/kbJuana4.webp`}
+            srcset={`../../images/collections/juanaPartOne/Juana4.webp`}
             type="image/webp"
           />
           <img
-            src={`../../images/collections/juanaPartOne/kbJuana4.png`}
+            src={`../../images/collections/juanaPartOne/Juana4.png`}
             alt="Juana part one 4"
             className="four-js abs"
           />
         </picture>
         <picture>
           <source
-            srcset={`../../images/collections/juanaPartOne/kbJuana5.webp`}
+            srcset={`../../images/collections/juanaPartOne/Juana5.webp`}
             type="image/webp"
           />
           <img
-            src={`../../images/collections/juanaPartOne/kbJuana5.png`}
+            src={`../../images/collections/juanaPartOne/Juana5.png`}
             alt="Juana part one 5"
             className="five-js abs"
           />
         </picture>
         <picture>
           <source
-            srcset={`../../images/collections/juanaPartOne/kbJuana6.webp`}
+            srcset={`../../images/collections/juanaPartOne/Juana6.webp`}
             type="image/webp"
           />
           <img
-            src={`../../images/collections/juanaPartOne/kbJuana6.png`}
+            src={`../../images/collections/juanaPartOne/Juana6.png`}
             alt="Juana part one 6"
             className="six-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana7.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana7.png`}
+            alt="Juana part one 7"
+            className="seven-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana8.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana8.png`}
+            alt="Juana part one 8"
+            className="eight-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana9.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana9.png`}
+            alt="Juana part one 9"
+            className="nine-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana10.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana10.png`}
+            alt="Juana part one 10"
+            className="ten-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana11.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana11.png`}
+            alt="Juana part one 11"
+            className="eleven-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana12.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana12.png`}
+            alt="Juana part one 12"
+            className="twelve-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana13.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana13.png`}
+            alt="Juana part one 13"
+            className="thirteen-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana14.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana14.png`}
+            alt="Juana part one 14"
+            className="fourteen-js abs"
+          />
+        </picture>
+        <picture>
+          <source
+            srcset={`../../images/collections/juanaPartOne/Juana15.webp`}
+            type="image/webp"
+          />
+          <img
+            src={`../../images/collections/juanaPartOne/Juana15.png`}
+            alt="Juana part one 15"
+            className="fifteen-js abs"
           />
         </picture>
         <HeaderFolio bgColor="bg-transparent z-50" />
