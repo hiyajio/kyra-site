c028fc07c1268d2a170789eb0baa95758eebaa33
lockjio <jio.buenviaje@gmail.com>
2020-06-17 14:30:50 -0400

    Make dropdown height responsive

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,7 +8,13 @@ const DropDownWrapper = styled.div`
   }
 
   .dropdown:hover .dropdown-content {
-    height: 6rem;
+    height: 7.5rem;
+  }
+
+  @media only screen and (min-width: 1680px) {
+    .dropdown:hover .dropdown-content {
+      height: 6rem;
+    }
   }
 `
 
@@ -20,26 +26,26 @@ const Dropdown = props => {
           <li className="hover:text-white hover:bg-silvered">/collections</li>
         </ul>
         <div className="dropdown-content relative overflow-hidden h-0">
-          <Link to="juanaPartOneC" aria-label="Juana 1 curated">
+          <Link to="/juanaPartOneC/" aria-label="Juana 1 curated">
             <ul>
               <li className="hover:text-white hover:bg-silvered">
                 juana part i - curated
               </li>
             </ul>
           </Link>
-          <Link to="juanaPartOne" aria-label="Juana 1 simple">
+          <Link to="/juanaPartOne/" aria-label="Juana 1 simple">
             <ul>
               <li className="hover:text-white hover:bg-silvered">
                 juana part i - 100% recycled denim
               </li>
             </ul>
           </Link>
-          <Link to="hold" aria-label="Hold">
+          <Link to="/hold/" aria-label="Hold">
             <ul>
               <li className="hover:text-white hover:bg-silvered">hold</li>
             </ul>
           </Link>
-          <Link to="spaceVegan" aria-label="Space vegan">
+          <Link to="/spaceVegan/" aria-label="Space vegan">
             <ul>
               <li className="hover:text-white hover:bg-silvered">
                 space vegan
