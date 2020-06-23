9c442e32cbc91b5c995300506b4393d12a17b40b
lockjio <jio.buenviaje@gmail.com>
2020-06-23 02:14:38 -0400

    Initialize gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,15 +13,22 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    /* {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
+    }, */
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
