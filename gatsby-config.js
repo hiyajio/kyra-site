7387aba5a816b4b52568965111339d936941f064
lockjio <jio.buenviaje@gmail.com>
2020-06-23 14:09:50 -0400

    Delete unused dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,16 +65,5 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: false, // Print removed selectors and processed file names
-        develop: false, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
   ],
 }
