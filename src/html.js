c93586974df991050630595705d23ea1beaa51eb
lockjio <jio.buenviaje@gmail.com>
2020-06-23 14:10:43 -0400

    Delete unused stylesheets

--- src/html.js
+++ src/html.js
@@ -13,17 +13,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link rel="shortcut icon" type="image/png" href={favicon} />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
