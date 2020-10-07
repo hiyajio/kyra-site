2609b55908729324b41d3cc5a812a78e6a9212d5
lockjio <jio.buenviaje@gmail.com>
2020-10-07 09:03:39 -0400

    Finish new press page

---
+++ src/pages/newPress.js
@@ -0,0 +1,289 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import HeaderFolio from "../components/HeaderFolio"
+import TopScroll from "../components/TopScroll"
+
+import "../styles/newPress.css"
+
+const NewPressPage = props => {
+  return (
+    <Layout>
+      <TopScroll>
+        <div
+          className={`flex bg-white font-body h-screen w-2/5 overflow-hidden`}
+        ></div>
+        <a
+          href="https://www.vogue.com/article/rhode-island-school-of-design-risd-class-of-2020"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="Vogue 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/vogue.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/vogue.png`}
+              alt="Vogue 1"
+              className="one-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://www.vogue.com/article/law-roach-supima-design-competition-interview"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="Vogue 2 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/vogue.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/vogue.png`}
+              alt="Vogue 1"
+              className="two-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://www.papermag.com/fashion-grads-cfda-2646067843"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="Paper 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/paper.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/paper.png`}
+              alt="Paper 1"
+              className="three-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://www.papermag.com/law-roach-supima-design-competition-2647042424"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="Paper 2 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/paper.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/paper.png`}
+              alt="Paper 2"
+              className="four-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://wwd.com/fashion-news/fashion-scoops/cfda-papermag-com-team-up-to-support-2020-fashion-school-grads-1203640022"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="WWD 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/wwd.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/wwd.png`}
+              alt="WWD 1"
+              className="five-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://wwd.com/business-news/business-features/supima-finalists-for-annual-design-competition-1202758508"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="WWD 2 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/wwd.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/wwd.png`}
+              alt="WWD 2"
+              className="six-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://www.thecut.com/2020/09/nyfw-designers-on-their-spring-summer-inspirations.html"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="The Cut 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/thecut.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/thecut.png`}
+              alt="The Cut 1"
+              className="seven-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://cfda.com/news/paper-cfda-celebrate-2020-graduates"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="CFDA 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/cfda.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/cfda.png`}
+              alt="CFDA 1"
+              className="eight-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://supima.com/design-competition/finalists/2020/Kyra%2520Buenviaje"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="Supima 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/supima.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/supima.png`}
+              alt="Supima 1"
+              className="nine-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://fashionista.com/page/supima-design-competition-2020-kyra-buenviaje-rhode-island-school-of-design"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="Fashionista 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/fashionista.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/fashionista.png`}
+              alt="Fashionista 1"
+              className="ten-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://fashionweekdaily.com/supima-design-competition-more-inspirations"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="The Daily Front Row 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/daily.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/daily.png`}
+              alt="The Daily Front Row 1"
+              className="eleven-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://daily-front-row-inc.foleon.com/supima-2020/2020-supima-design-competition/kyra-buenviaje"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="The Daily Front Row 2 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/daily.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/daily.png`}
+              alt="The Daily Front Row 2"
+              className="twelve-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://www.risd.edu/news/stories/apparel-design-graduate-kyra-buenviaje-competes-in-supima-design-competition-2020"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="RISD 1 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/risd.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/risd.png`}
+              alt="RISD 1"
+              className="thirteen-npr abs"
+            />
+          </picture>
+        </a>
+        <a
+          href="https://publications.risdmuseum.org/risd-senior-show-2020-apparel-design/kyra-buenviaje-apparel-design-risd-senior-show-2020"
+          rel="noopener noreferrer"
+          target="_BLANK"
+          className="opacity-50 hover:opacity-100"
+          aria-label="RISD 2 link"
+        >
+          <picture>
+            <source
+              srcset={`../../images/collections/press/risd.webp`}
+              type="image/webp"
+            />
+            <img
+              src={`../../images/collections/press/risd.png`}
+              alt="RISD 2"
+              className="fourteen-npr abs"
+            />
+          </picture>
+        </a>
+        <div className="flex-1 bg-white font-body h-screen w-2/5 overflow-hidden"></div>
+        <HeaderFolio bgColor="bg-transparent z-50" />
+      </TopScroll>
+    </Layout>
+  )
+}
+
+export default NewPressPage
