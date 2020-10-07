baeabd5cea2bd350980a1d6e7f866f1ce1d0e638
lockjio <jio.buenviaje@gmail.com>
2020-10-07 06:17:28 -0400

    Fix overwriting of styling for new collection

--- src/pages/juana.js
+++ src/pages/juana.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 import HeaderFolio from "../components/HeaderFolio"
 import TopScroll from "../components/TopScroll"
 
-import "../styles/juana.css"
+import "../styles/juanaFinal.css"
 
 const JuanaPage = props => {
   return (
@@ -21,7 +21,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana1.jpg`}
             alt="Juana part one curated 1"
-            className="one-jc"
+            className="one-jf"
           />
         </picture>
         <picture>
@@ -32,7 +32,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana2.jpg`}
             alt="Juana part one curated 2"
-            className="two-jc abs"
+            className="two-jf abs"
           />
         </picture>
         <picture>
@@ -43,7 +43,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana3.jpg`}
             alt="Juana part one curated 3"
-            className="three-jc abs"
+            className="three-jf abs"
           />
         </picture>
         <picture>
@@ -54,7 +54,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana4.jpg`}
             alt="Juana part one curated 4"
-            className="four-jc"
+            className="four-jf"
           />
         </picture>
         <picture>
@@ -65,7 +65,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana5.jpg`}
             alt="Juana part one curated 5"
-            className="five-jc abs"
+            className="five-jf abs"
           />
         </picture>
         <picture>
@@ -76,7 +76,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana6.jpg`}
             alt="Juana part one curated 6"
-            className="six-jc abs"
+            className="six-jf abs"
           />
         </picture>
         <picture>
@@ -87,7 +87,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana7.jpg`}
             alt="Juana part one curated 7"
-            className="seven-jc"
+            className="seven-jf"
           />
         </picture>
         <picture>
@@ -98,7 +98,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana8.jpg`}
             alt="Juana part one curated 8"
-            className="eight-jc abs"
+            className="eight-jf abs"
           />
         </picture>
         <picture>
@@ -109,7 +109,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana9.jpg`}
             alt="Juana part one curated 9"
-            className="nine-jc abs"
+            className="nine-jf abs"
           />
         </picture>
         <picture>
@@ -120,7 +120,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana10.jpg`}
             alt="Juana part one curated 10"
-            className="ten-jc abs"
+            className="ten-jf abs"
           />
         </picture>
         <picture>
@@ -131,7 +131,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana11.jpg`}
             alt="Juana part one curated 11"
-            className="eleven-jc abs"
+            className="eleven-jf abs"
           />
         </picture>
         <picture>
@@ -142,7 +142,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana12.jpg`}
             alt="Juana part one curated 12"
-            className="twelve-jc abs"
+            className="twelve-jf abs"
           />
         </picture>
         <picture>
@@ -153,7 +153,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana13.jpg`}
             alt="Juana part one curated 13"
-            className="thirteen-jc abs"
+            className="thirteen-jf abs"
           />
         </picture>
         <picture>
@@ -164,7 +164,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana14.jpg`}
             alt="Juana part one curated 14"
-            className="fourteen-jc abs"
+            className="fourteen-jf abs"
           />
         </picture>
         <picture>
@@ -175,7 +175,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana15.jpg`}
             alt="Juana part one curated 15"
-            className="fifteen-jc abs"
+            className="fifteen-jf abs"
           />
         </picture>
         <picture>
@@ -186,7 +186,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana16.jpg`}
             alt="Juana part one curated 16"
-            className="sixteen-jc abs"
+            className="sixteen-jf abs"
           />
         </picture>
         <picture>
@@ -197,7 +197,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana17.jpg`}
             alt="Juana part one curated 17"
-            className="seventeen-jc abs"
+            className="seventeen-jf abs"
           />
         </picture>
         <picture>
@@ -208,7 +208,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana18.jpg`}
             alt="Juana part one curated 18"
-            className="eighteen-jc abs"
+            className="eighteen-jf abs"
           />
         </picture>
         <picture>
@@ -219,7 +219,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana19.jpg`}
             alt="Juana part one curated 19"
-            className="nineteen-jc abs"
+            className="nineteen-jf abs"
           />
         </picture>
         <picture>
@@ -230,7 +230,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana20.jpg`}
             alt="Juana part one curated 20"
-            className="twenty-jc abs"
+            className="twenty-jf abs"
           />
         </picture>
         <picture>
@@ -241,7 +241,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana21.jpg`}
             alt="Juana part one curated 21"
-            className="twentyone-jc abs"
+            className="twentyone-jf abs"
           />
         </picture>
         <picture>
@@ -252,7 +252,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana22.jpg`}
             alt="Juana part one curated 22"
-            className="twentytwo-jc abs"
+            className="twentytwo-jf abs"
           />
         </picture>
         <picture>
@@ -263,7 +263,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana23.jpg`}
             alt="Juana part one curated 23"
-            className="twentythree-jc abs"
+            className="twentythree-jf abs"
           />
         </picture>
         <picture>
@@ -274,7 +274,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana24.jpg`}
             alt="Juana part one curated 24"
-            className="twentyfour-jc abs"
+            className="twentyfour-jf abs"
           />
         </picture>
         <picture>
@@ -285,7 +285,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana25.jpg`}
             alt="Juana part one curated 25"
-            className="twentyfive-jc abs"
+            className="twentyfive-jf abs"
           />
         </picture>
         <picture>
@@ -296,7 +296,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana26.jpg`}
             alt="Juana part one curated 26"
-            className="twentysix-jc abs"
+            className="twentysix-jf abs"
           />
         </picture>
         <picture>
@@ -307,7 +307,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana27.jpg`}
             alt="Juana part one curated 27"
-            className="twentyseven-jc abs"
+            className="twentyseven-jf abs"
           />
         </picture>
         <picture>
@@ -318,7 +318,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana28.jpg`}
             alt="Juana part one curated 28"
-            className="twentyeight-jc abs"
+            className="twentyeight-jf abs"
           />
         </picture>
         <picture>
@@ -329,7 +329,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana29.jpg`}
             alt="Juana part one curated 29"
-            className="twentynine-jc abs"
+            className="twentynine-jf abs"
           />
         </picture>
         <picture>
@@ -340,7 +340,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana30.jpg`}
             alt="Juana part one curated 30"
-            className="thirty-jc"
+            className="thirty-jf"
           />
         </picture>
         <picture>
@@ -351,7 +351,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana31.jpg`}
             alt="Juana part one curated 31"
-            className="thirtyone-jc abs"
+            className="thirtyone-jf abs"
           />
         </picture>
         <picture>
@@ -362,7 +362,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana32.jpg`}
             alt="Juana part one curated 32"
-            className="thirtytwo-jc abs"
+            className="thirtytwo-jf abs"
           />
         </picture>
         <picture>
@@ -373,7 +373,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana33.jpg`}
             alt="Juana part one curated 33"
-            className="thirtythree-jc abs"
+            className="thirtythree-jf abs"
           />
         </picture>
         <picture>
@@ -384,7 +384,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana34.jpg`}
             alt="Juana part one curated 34"
-            className="thirtyfour-jc abs"
+            className="thirtyfour-jf abs"
           />
         </picture>
         <picture>
@@ -395,7 +395,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana35.jpg`}
             alt="Juana part one curated 35"
-            className="thirtyfive-jc abs"
+            className="thirtyfive-jf abs"
           />
         </picture>
         <picture>
@@ -406,7 +406,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana36.jpg`}
             alt="Juana part one curated 36"
-            className="thirtysix-jc abs"
+            className="thirtysix-jf abs"
           />
         </picture>
         <picture>
@@ -417,7 +417,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana37.jpg`}
             alt="Juana part one curated 37"
-            className="thirtyseven-jc abs"
+            className="thirtyseven-jf abs"
           />
         </picture>
         <picture>
@@ -428,7 +428,7 @@ const JuanaPage = props => {
           <img
             src={`../../images/collections/juana/FJuana38.jpg`}
             alt="Juana part one curated 38"
-            className="thirtyeight-jc abs"
+            className="thirtyeight-jf abs"
           />
         </picture>
         <div className="flex-1 bg-white font-body h-screen w-2/5 overflow-hidden"></div>
