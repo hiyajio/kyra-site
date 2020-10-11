5342b1c5504d1f076ce18bdaa5889781253ecab8
lockjio <jio.buenviaje@gmail.com>
2020-10-10 23:07:36 -0400

    Add xxl screen config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ module.exports = {
         bronzed: "#3a3633",
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'xxl': '1560px',
+      // => @media (mid-width: 1560px) { ... }
+    }
   },
   purge: false,
   variants: {},
