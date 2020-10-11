cfafed828ca169a7d3db389a6b743370bf572c2c
lockjio <jio.buenviaje@gmail.com>
2020-10-10 23:09:02 -0400

    Apply new header to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from "react"
 // import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import HeaderVideo from "../components/HeaderVideo"
+import HeaderHome from "../components/HeaderHome"
 
 import "../styles/index.css"
 
@@ -27,7 +27,7 @@ const IndexPage = props => {
           </picture>
         </div>
         <div className="absolute right-0">
-          <HeaderVideo bgColor="bg-transparent z-50 pr-40" />
+          <HeaderHome bgColor="bg-transparent z-50 pr-40" />
         </div>
       </div>
     </Layout>
